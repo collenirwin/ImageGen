d7b0f8d0f25ef0d9dc0fed980ea737e92f036f28
Collen Irwin <collenirwin@gmail.com>
2017-08-31 16:01:22 -0400

    Initial Commit

---
+++ ImageGen/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ImageGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnForeColor = new System.Windows.Forms.Button();
+            this.chkRandom = new System.Windows.Forms.CheckBox();
+            this.btnBackColor = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.picture.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.picture.Location = new System.Drawing.Point(80, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(300, 300);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(233, 318);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(60, 39);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Gen";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(299, 318);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(60, 39);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnForeColor
+            // 
+            this.btnForeColor.Location = new System.Drawing.Point(101, 318);
+            this.btnForeColor.Name = "btnForeColor";
+            this.btnForeColor.Size = new System.Drawing.Size(60, 39);
+            this.btnForeColor.TabIndex = 3;
+            this.btnForeColor.Text = "Fore Color";
+            this.btnForeColor.UseVisualStyleBackColor = true;
+            this.btnForeColor.Click += new System.EventHandler(this.btnForeColor_Click);
+            // 
+            // chkRandom
+            // 
+            this.chkRandom.AutoSize = true;
+            this.chkRandom.Location = new System.Drawing.Point(101, 363);
+            this.chkRandom.Name = "chkRandom";
+            this.chkRandom.Size = new System.Drawing.Size(108, 19);
+            this.chkRandom.TabIndex = 4;
+            this.chkRandom.Text = "Random Colors";
+            this.chkRandom.UseVisualStyleBackColor = true;
+            this.chkRandom.CheckedChanged += new System.EventHandler(this.chkRandom_CheckedChanged);
+            // 
+            // btnBackColor
+            // 
+            this.btnBackColor.Location = new System.Drawing.Point(167, 318);
+            this.btnBackColor.Name = "btnBackColor";
+            this.btnBackColor.Size = new System.Drawing.Size(60, 39);
+            this.btnBackColor.TabIndex = 5;
+            this.btnBackColor.Text = "Back Color";
+            this.btnBackColor.UseVisualStyleBackColor = true;
+            this.btnBackColor.Click += new System.EventHandler(this.btnBackColor_Click);
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.AnyColor = true;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "bmp";
+            this.saveFileDialog.FileName = "MyImage";
+            this.saveFileDialog.Filter = "Bitmap files|*.bmp";
+            this.saveFileDialog.Title = "Save - ImageGen";
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(461, 392);
+            this.Controls.Add(this.btnBackColor);
+            this.Controls.Add(this.chkRandom);
+            this.Controls.Add(this.btnForeColor);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.picture);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ImageGen";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnForeColor;
+        private System.Windows.Forms.CheckBox chkRandom;
+        private System.Windows.Forms.Button btnBackColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ ImageGen/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageGen
+{
+    public partial class Form1 : Form
+    {
+        #region Vars
+
+        const int NUM_COLLUMNS = 6;
+        const int DRAW_CHANCE = 50;
+
+        Random ran = new Random();
+
+        Color foreColor = Color.MediumSeaGreen;
+        Color backColor = Color.White;
+
+        Bitmap bmp;
+
+        #endregion
+
+        #region Methods
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnForeColor.BackColor = foreColor;
+            btnBackColor.BackColor = backColor;
+        }
+
+        #region Coloring
+
+        private void btnForeColor_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+            foreColor = colorDialog.Color;
+            btnForeColor.BackColor = foreColor;
+        }
+
+        private void btnBackColor_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+            backColor = colorDialog.Color;
+            btnBackColor.BackColor = backColor;
+        }
+
+        private void chkRandom_CheckedChanged(object sender, EventArgs e)
+        {
+            btnForeColor.Enabled = !chkRandom.Checked;
+            btnBackColor.Enabled = !chkRandom.Checked;
+        }
+
+        #endregion
+
+        #region Drawing
+
+        private Color randomColor()
+        {
+            return Color.FromArgb(255, ran.Next(256), ran.Next(256), ran.Next(256));
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            var tempBmp = new Bitmap(picture.Width / 2, picture.Height);
+
+            using (var graphics = Graphics.FromImage(tempBmp))
+            {
+                Brush foreBrush;
+                Brush backBrush;
+
+                // determine brush colors
+                if (chkRandom.Checked)
+                {
+                    foreBrush = new SolidBrush(randomColor());
+                    backBrush = new SolidBrush(randomColor());
+                }
+                else
+                {
+                    foreBrush = new SolidBrush(foreColor);
+                    backBrush = new SolidBrush(backColor);
+                }
+
+
+                // draw background
+                graphics.FillRectangle(
+                    backBrush, // color
+                    0, // x
+                    0, // y
+                    tempBmp.Width, // width
+                    tempBmp.Height // height
+                );
+
+                // set box dimensions and init x and y
+                int boxWidth = picture.Width / NUM_COLLUMNS;
+                int boxHeight = picture.Height / NUM_COLLUMNS;
+                int xPos = 0;
+                int yPos = 0;
+                
+                for (int x = 0; x < NUM_COLLUMNS; x++)
+                {
+                    for (int y = 0; y < NUM_COLLUMNS; y++)
+                    {
+                        // if we roll to draw a rectangle
+                        if (ran.Next(1, 101) <= DRAW_CHANCE)
+                        {
+                            graphics.FillRectangle(foreBrush, xPos, yPos, boxWidth, boxHeight);
+                        }
+
+                        // go down
+                        yPos += boxHeight;
+                    }
+
+                    // go right
+                    xPos += boxWidth;
+
+                    // reset Y
+                    yPos = 0;
+                }
+            }
+            // flip tempBmp horizontally, put it into bmpFlipped
+            var tempBmpFlipped = new Bitmap(tempBmp);
+            tempBmpFlipped.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            // create bmp with picture's dimensions
+            bmp = new Bitmap(picture.Width, picture.Height);
+
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                // draw tempBmp on the left
+                graphics.DrawImage(tempBmp, 0, 0);
+
+                // draw tempBmpFlipped on the right
+                graphics.DrawImage(tempBmpFlipped, bmp.Width / 2, 0);
+            }
+            
+            // display our beautiful bmp in picture.image
+            picture.Image = bmp;
+        }
+
+        #endregion
+
+        #region Saving
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                saveFileDialog.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Please generate an image first.");
+                btnGen.Focus();
+            }
+        }
+
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                bmp.Save(saveFileDialog.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Failed to save your image with the following exception:\n" +
+                    ex.Message
+                );
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
